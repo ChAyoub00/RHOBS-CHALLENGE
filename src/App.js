f15e10f4cdd2d7b9ddfd0a6ee57501b5998d992c
AyouubCh <ayoubchadifii@gmail.com.com>
2024-02-16 00:52:19 +0100

    Using react hook form

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React, { useState, useEffect } from 'react';
-import './App.css'; // Importation du fichier de styles App.css
+import { useForm } from 'react-hook-form';
+import './App.css'; 
 
 const App = () => {
-  const [bodies, setBodies] = useState([]); // Liste de tous les corps célestes
-  const [selectedBody, setSelectedBody] = useState(null); // Corps céleste sélectionné
-  const [filteredBodies, setFilteredBodies] = useState([]); // Corps célestes filtrés en fonction des critères
-  const [isPlanetChecked, setIsPlanetChecked] = useState(false); // Indique si la case à cocher "Est une planète" est cochée
-  const [gravity, setGravity] = useState(0);  // Valeur de la gravité pour filtrer les corps célestes
+  const {register, watch} = useForm();
+  const [bodies, setBodies] = useState([]); 
+  const [selectedBody, setSelectedBody] = useState(null); 
+  const [filteredBodies, setFilteredBodies] = useState([]); 
 
   useEffect(() => {
     fetchBodies();
@@ -23,48 +23,40 @@ const fetchBodies = async () => {
     }
   };
 
-// Fonction de filtrage des corps célestes
-const filterBodies = () => {
-    let filtered = bodies; 
-
-    if (isPlanetChecked) {
-      filtered = filtered.filter(body => body.isPlanet); // Si  on a coché le checkbox "isPlanet", on filtre sur les planètes
-    }
-
-    filtered = filtered.filter(body => body.gravity <= gravity); //  Filtre sur la gravité
-
-    setFilteredBodies(filtered); //  Met à jour l'état filteredBodies
-  };
-
   useEffect(() => {
     filterBodies();
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [isPlanetChecked, gravity]);
+  }, [watch("isPlanetChecked"), watch("gravity")]);
+
+  const filterBodies = () => {
+    let filtered = bodies.slice(); 
+
+    if (watch("isPlanetChecked")) {
+      filtered = filtered.filter(body => body.isPlanet);
+    }
+
+    filtered = filtered.filter(body => body.gravity <= watch("gravity"));
+
+    setFilteredBodies(filtered);
+  };
 
   const handleBodySelect = (body) => { //  Gère le clic sur un corps céleste
     setSelectedBody(body);
-  };
-
-  const handlePlanetCheckboxChange = (event) => { // Gère le changement d'état du checkbox "isPlanet"
-    setIsPlanetChecked(event.target.checked);
-  };
-
-  const handleGravityChange = (event) => { // Gère le changement de valeur de gravité
-    setGravity(parseFloat(event.target.value));
   };
 
   return (
     <div className="app">
       <h1 className="header">RHOBS Challenge</h1>
       <div className="form-container">
+        <form>
           <label className="form-label">
             Is Planet:
-            <input type="checkbox" checked={isPlanetChecked} onChange={handlePlanetCheckboxChange} />
+            <input type="checkbox" {...register("isPlanetChecked")} />
           </label>
           <label className="form-label">
             Gravity:
-            <input type="range" value={gravity} onChange={handleGravityChange} />
+            <input type="range" {...register("gravity")} />
           </label>
+        </form>
       </div>
       <div className="select-container">
         <label className="select-label"> Bodies: 
